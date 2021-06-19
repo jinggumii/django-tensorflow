c9e49d9e3d2ace5e85daa4268ea99dbeb348b07b
jinggumii <jinggumi9@gmail.com>
2021-06-19 17:52:55 +0900

    final commit

--- tf_sample/calculator.py
+++ tf_sample/calculator.py
@@ -31,3 +31,33 @@ class Service(object):
 
     @tf.function
     def div(self, num1, num2): return tf.divide(num1, num2)
+
+
+if __name__ == '__main__':
+    model = Model()
+    service = Service()
+
+    def print_menu():
+        print('0. 종료')
+        print('+')
+        print('-')
+        print('*')
+        print('/')
+        return input('메뉴 입력\n')
+
+    while 1:
+        num1 = input('숫자1 입력\n')
+        a = tf.constant(int(num1))
+        menu = print_menu()
+        num2 = input('숫자2 입력\n')
+        b = tf.constant(int(num2))
+        if menu == '+':
+            print('a + b = {}'.format(service.plus(a, b)))
+        if menu == '-':
+            pass
+        if menu == '*':
+            pass
+        if menu == '/':
+            pass
+        elif menu == '0':
+            pass
